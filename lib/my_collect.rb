592b36107b84280d9d4a92c383a7b8251d4404eb
Emilio Betancourt <embeor@gmail.com>
2018-10-06 20:34:23 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,16 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+
+
 def my_collect (argument)
   i = 0
   while argument.length > i
-
+    yield argument[i]
     i +=1
 
   end
   argument
+end
+
+my_collect(array) do |name|
+  name.split(" ").first
 end
